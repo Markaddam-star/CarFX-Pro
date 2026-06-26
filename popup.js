4b92c0e74399b69ac89d1dd235f9238e2abc1308
Markaddam-star <markaddam77@outlook.com>
2026-06-26 23:52:04 +0500

    feat: add popup.js settings logic

--- popup.js
+++ popup.js
@@ -1,1 +1,34 @@
 
+document.addEventListener("DOMContentLoaded", () => {
+
+    const enabled = document.getElementById("enabled");
+    const interval = document.getElementById("interval");
+    const save = document.getElementById("save");
+
+    chrome.storage.local.get("carfx", (data) => {
+
+        if (data.carfx) {
+            enabled.checked = data.carfx.enabled;
+            interval.value = data.carfx.interval;
+        }
+
+    });
+
+    save.addEventListener("click", () => {
+
+        chrome.storage.local.set({
+
+            carfx: {
+                enabled: enabled.checked,
+                interval: Number(interval.value)
+            }
+
+        }, () => {
+
+            alert("Settings Saved!");
+
+        });
+
+    });
+
+});
