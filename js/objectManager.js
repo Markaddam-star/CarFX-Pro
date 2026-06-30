7ea97e4f0fbfa5851eb4153c92d8be436500dd84
Markaddam-star <markaddam77@outlook.com>
2026-06-30 19:22:51 +0500

    feat(objects): add object manager
    Implemented ObjectManager class with methods to add, remove, update, render, clear, and count objects.

--- js/objectManager.js
+++ js/objectManager.js
@@ -1,1 +1,111 @@
 
+/**
+ * ============================================================
+ * CarFX Pro Ultimate
+ * Object Manager v0.2
+ * ============================================================
+ */
+
+class ObjectManager {
+
+    constructor() {
+        this.objects = [];
+    }
+
+    /**
+     * Add a new object
+     */
+    add(object) {
+
+        if (!object) return;
+
+        this.objects.push(object);
+
+        if (typeof object.init === "function") {
+            object.init();
+        }
+
+    }
+
+    /**
+     * Remove object
+     */
+    remove(object) {
+
+        const index = this.objects.indexOf(object);
+
+        if (index !== -1) {
+
+            if (typeof object.destroy === "function") {
+                object.destroy();
+            }
+
+            this.objects.splice(index, 1);
+
+        }
+
+    }
+
+    /**
+     * Update all objects
+     */
+    update(delta) {
+
+        for (const object of this.objects) {
+
+            if (typeof object.update === "function") {
+
+                object.update(delta);
+
+            }
+
+        }
+
+    }
+
+    /**
+     * Render all objects
+     */
+    render(renderer) {
+
+        for (const object of this.objects) {
+
+            if (typeof object.render === "function") {
+
+                object.render(renderer);
+
+            }
+
+        }
+
+    }
+
+    /**
+     * Destroy everything
+     */
+    clear() {
+
+        for (const object of this.objects) {
+
+            if (typeof object.destroy === "function") {
+
+                object.destroy();
+
+            }
+
+        }
+
+        this.objects = [];
+
+    }
+
+    /**
+     * Total objects
+     */
+    count() {
+
+        return this.objects.length;
+
+    }
+
+}
